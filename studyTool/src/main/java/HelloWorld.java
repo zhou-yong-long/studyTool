7c037bc0350c8769f04a753bc2971c15a87f9278
zhou-yong-long <54322222+zhou-yong-long@users.noreply.github.com>
2021-01-17 17:15:08 +0800

    Delete studyTool directory

--- studyTool/src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author wade
- * @creat 2021-01-17-2021/1/17
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
