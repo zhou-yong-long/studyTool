0774c6da747397815338f9d99d3e8be7752a0eca
zhou-yong-long <zhouyonglong09@126.com>
2021-01-17 16:57:43 +0800

    测试

---
+++ studyTool/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author wade
+ * @creat 2021-01-17-2021/1/17
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
